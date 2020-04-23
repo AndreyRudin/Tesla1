d0201e365f1241dc244d8e6d661a1a206c55400e
Andrey Rudin <rudin1996@mail.ru>
2020-04-23 14:50:06 +0300

    доделал 2 часть

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,10 @@
 $(function(){
     $('.slider').slick({
-        prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
-        nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
-            fade: true,
-            responsive: [{
-                breakpoint: 441,
-                settings: {
-                    arrows:false
-                }
-            }
-        ]
+        fade: true,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:3000,
+        dots: true
     });
 });
 
